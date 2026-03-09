169de77dfee6966d4dd00025e33dffe75ded5c7e
Aliza-kanwal <alizakanwal408@gmail.com>
2026-03-10 01:15:01 +0500

    Fix import statement for processHourlyForecast

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {
   fetchWeatherByCoords,
   fetchForecastByCoords,
   processForecastData,
-  processHourlyForecast,
+  processHourlyForecast
 } from './services/weatherService';
 
 function App() {
